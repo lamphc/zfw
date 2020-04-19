75358b13d9527d8b4aa0fff4efbd94417542f647
lamphc <lamphc@live.com>
2020-04-19 15:33:14 +0800

    按需加载和路由配置

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom'
+import Home from './pages/Home';
+import CityList from './pages/CityList';
+import Map from './pages/Map';
+import NotFound from './pages/NotFound';
+
 
 function App() {
   return (
-    <div className="app">
-      app100
-    </div>
+    <Router>
+      <div className="app">
+        <Link to="/home">首页</Link>
+        <Link to="/cityList">城市列表</Link>
+        <Link to="/map">地图找房</Link>
+        <Switch>
+          {/* 路由重定向 */}
+          <Redirect from="/" to="/home" />
+          {/* 应用的一级路由 */}
+          <Route path="/home" component={Home} />
+          <Route path="/cityList" component={CityList} />
+          <Route path="/map" component={Map} />
+          {/* 配置404页面 */}
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
