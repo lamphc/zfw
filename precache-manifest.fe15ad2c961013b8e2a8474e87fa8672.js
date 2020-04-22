34079dc80a87c160adc3650ae28df7e22d023f8e
lamphc <lamphc@live.com>
2020-04-22 15:59:10 +0800

    Updates

---
+++ precache-manifest.fe15ad2c961013b8e2a8474e87fa8672.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824eb77f60ab9d340a9b0b260b698339",
+    "url": "/zfw/index.html"
+  },
+  {
+    "revision": "513499990b88a99443d4",
+    "url": "/zfw/static/css/2.38aff980.chunk.css"
+  },
+  {
+    "revision": "8dc1adabc0e0c4ff886e",
+    "url": "/zfw/static/css/main.1d57d070.chunk.css"
+  },
+  {
+    "revision": "513499990b88a99443d4",
+    "url": "/zfw/static/js/2.e1ae485f.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/zfw/static/js/2.e1ae485f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc1adabc0e0c4ff886e",
+    "url": "/zfw/static/js/main.49ed6fe7.chunk.js"
+  },
+  {
+    "revision": "89413d9a9983743c0b10",
+    "url": "/zfw/static/js/runtime-main.54306f84.js"
+  },
+  {
+    "revision": "3b538f76f922de087df0eb82f703119a",
+    "url": "/zfw/static/media/iconfont.3b538f76.ttf"
+  },
+  {
+    "revision": "80483e3b898177297ad8b08c1e002f55",
+    "url": "/zfw/static/media/iconfont.80483e3b.eot"
+  },
+  {
+    "revision": "c931e1ae5f45a521413c9905641f2071",
+    "url": "/zfw/static/media/iconfont.c931e1ae.svg"
+  },
+  {
+    "revision": "f58b610e9d4e6470d644d7d195bbfda6",
+    "url": "/zfw/static/media/iconfont.f58b610e.woff"
+  }
+]);
