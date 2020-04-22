e105d9f2ba3a17f4e2bfb67b36848c2e55fc4ea2
lamphc <lamphc@live.com>
2020-04-22 15:50:53 +0800

    百度地图和debugger配置

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,10 +1,44 @@
+/**
+ * 地图找房
+ */
 import React, { Component } from 'react';
+import './index.scss'
+import { NavBar, Icon } from 'antd-mobile';
+
+
 
 class Map extends Component {
+
+  componentDidMount() {
+    this.initMap()
+  }
+
+  // 初始化地图
+  initMap = () => {
+    const { BMap } = window;
+    // 打断点
+    // console.log(BMap)
+    // 1. 创建地图实例
+    const map = new BMap.Map("container");
+    // 2. 地图定位的经纬度设置(天安门)
+    let point = new BMap.Point(116.404, 39.915);
+    // 3. 设置地图的位置和缩放级别
+    map.centerAndZoom(point, 15);
+  }
+
+
   render() {
     return (
-      <div>
-        Map
+      <div className="mapBox">
+        {/* 导航栏 */}
+        <NavBar
+          mode="dark"
+          icon={<Icon type="left" />}
+          onLeftClick={() => this.props.history.goBack()}
+        >地图找房</NavBar>
+
+        {/* 地图 */}
+        <div id="container"></div>
       </div>
     );
   }
