65b8975abea46a9dfd825237168cf099358cc874
lamphc <lamphc@live.com>
2020-04-26 11:10:50 +0800

    前三个筛选器功能OK

--- src/pages/House/components/Filter/index.js
+++ src/pages/House/components/Filter/index.js
@@ -20,6 +20,15 @@ const titleSelectedStatus = {
   more: false
 }
 
+// 当前选中的picker值的默认数据
+const selectedValues = {
+  // 没有选中值（默认值）
+  area: ["area", "null"], // 包含区域和地铁
+  mode: ["null"],
+  price: ["null"],
+  more: []
+}
+
 export default class Filter extends Component {
   // 定义状态数据
   state = {
@@ -30,7 +39,9 @@ export default class Filter extends Component {
   }
 
   componentDidMount() {
-    this.getFilterData()
+    this.getFilterData();
+    // 当前选中的值
+    this.selectedValues = { ...selectedValues };
   }
 
 
@@ -41,7 +52,8 @@ export default class Filter extends Component {
     let { status, data } = await getFilters(value);
     if (status === 200) {
       // 把数据存储到this上
-      this.filterDatas = data
+      this.filterDatas = data;
+      console.log('过滤器所有的数据：', this.filterDatas)
     }
   }
 
@@ -63,18 +75,101 @@ export default class Filter extends Component {
     return openType === 'area' || openType === 'mode' || openType === 'price'
   }
 
+  // 处理确定的时候，查询selectedValues对应的选择器是否有数据 =》 高亮对应的title
+  handlerSel = () => {
+    // 存储新的高亮状态
+    const newStatus = { ...titleSelectedStatus };
+    // 遍历存储的选中数据，确定是否高亮
+    Object.keys(this.selectedValues).forEach((key) => {
+      // 获取当前 picker选中的值
+      let cur = this.selectedValues[key];
+      // 判断是否高亮
+      if (key === 'area' && (cur[1] !== 'null' || cur[0] === 'subway')) {
+        newStatus[key] = true
+      } else if (key === 'mode' && cur[0] !== 'null') {
+        newStatus[key] = true
+      } else if (key === 'price' && cur[0] !== 'null') {
+        newStatus[key] = true
+      } else {
+        newStatus[key] = false
+      }
+
+    })
+
+    return newStatus;
+
+  }
+
   // 点击确定的时候执行
-  onOk = () => {
+  onOk = (curSel) => {
+    console.log('picker当前选中的值：', curSel)
+    // 存储到组件this（实例）
+    const { openType } = this.state;
+    this.selectedValues[openType] = curSel;
+
+
     this.setState({
-      openType: ''
+      openType: '',
+      // 处理高亮状态
+      titleSelectedStatus: this.handlerSel()
     })
   }
 
   // 点击取消的时候
   onCancle = () => {
     this.setState({
-      openType: ''
+      openType: '',
+      // 处理高亮状态
+      titleSelectedStatus: this.handlerSel()
     })
+  }
+
+  // 渲染picker并提供对应的数据
+  renderPicker = () => {
+    if (this.isShowPicker()) {
+      // 获取对应picker的数据
+      const { area, subway, rentType, price } = this.filterDatas;
+      const { openType } = this.state;
+      // 传递对应的picker数据
+      let data, cols = 1;
+      // 当前选中的值
+      let curSel = this.selectedValues[openType];
+      // 根据openType去取当前点击的picker数据
+      switch (openType) {
+        case 'area':
+          data = [area, subway];
+          cols = 3;
+          break;
+        case 'mode':
+          data = rentType
+          break;
+        case 'price':
+          data = price
+          break;
+      }
+
+      return <FilterPicker data={data} key={openType} value={curSel} cols={cols} onOk={this.onOk} onCancle={this.onCancle} />
+    }
+  }
+
+  // 渲染第四个筛选器
+  renderFilterMore = () => {
+    const { openType } = this.state;
+    if (openType === 'more') {
+      // 传递后台过滤条件的数据
+      console.log(this.filterDatas);
+      const { oriented, floor, roomType, characteristic } = this.filterDatas;
+      let data = { oriented, floor, roomType, characteristic };
+      return (
+        <FilterMore
+          data={data}
+          onOk={this.onOk}
+          onCancle={this.onCancle}
+
+        />
+      )
+    }
+
   }
 
   render() {
@@ -91,11 +186,13 @@ export default class Filter extends Component {
 
           {/* 前三个菜单对应的内容： */}
           {
-            this.isShowPicker() ? <FilterPicker onOk={this.onOk} onCancle={this.onCancle} /> : null
+            this.renderPicker()
           }
 
           {/* 最后一个菜单对应的内容： */}
-          {/* <FilterMore /> */}
+          {
+            this.renderFilterMore()
+          }
         </div>
       </div>
     )
