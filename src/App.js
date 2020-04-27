cfe34ef30654b02dd9153c77a364bd6a88dd12be
lamphc <lamphc@live.com>
2020-04-27 11:06:52 +0800

    feat: 列表找房下拉加载和详情展示

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import CityList from './pages/CityList';
 import Map from './pages/Map';
 import NotFound from './pages/NotFound';
+import HouseDetail from './components/HouseDetail';
 
 
 function App() {
@@ -20,6 +21,8 @@ function App() {
           <Route path="/cityList" component={CityList} />
           {/* 地图找房 */}
           <Route path="/map" component={Map} />
+          {/* 房源详情的路由 */}
+          <Route path="/detail/:id" component={HouseDetail} />
           {/* 配置404页面 */}
           <Route component={NotFound} />
         </Switch>
--- src/pages/House/index.js
+++ src/pages/House/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Flex } from 'antd-mobile'
+import { Flex, Toast } from 'antd-mobile'
 
 import Filter from './components/Filter'
 // import './name.scss'
@@ -9,9 +9,10 @@ import styles from './index.module.css'
 import { getListByFilter } from '../../utils/api/House'
 import { getCurCity } from '../../utils'
 
-import { List, AutoSizer } from 'react-virtualized';
+import { List, AutoSizer, InfiniteLoader } from 'react-virtualized';
 import HouseItem from '../../components/HouseItem'
 import { BASE_URL } from '../../utils/axios'
+import NoHouse from '../../components/NoHouse'
 
 
 export default class HouseList extends React.Component {
@@ -20,7 +21,9 @@ export default class HouseList extends React.Component {
   // state 设置的是=》会引起html变化的数据 =》diff 虚拟DOM：jsx+state
   state = {
     // 房屋列表数据
-    list: []
+    list: [],
+    // 列表数据的总条数
+    count: 0
   }
 
   // 渲染列表项方法
@@ -35,10 +38,20 @@ export default class HouseList extends React.Component {
     const { list } = this.state;
     // 获取当前列表项的数据
     const item = list[index];
+    // 数据item没有的时候
+    if (!item) {
+      return (
+        <div style={style} key={key}>
+          <p className={styles.loading}></p>
+        </div>
+      )
+    }
     // 处理图片地址和键
     item.src = `${BASE_URL}${item.houseImg}`
     return (
-      <HouseItem {...item} key={key} style={style} />
+      <HouseItem {...item} key={key} onClick={() => {
+        this.props.history.push('/detail/' + item.houseCode)
+      }} style={style} />
     );
   }
 
@@ -63,12 +76,64 @@ export default class HouseList extends React.Component {
 
   // 获取列表数据
   getHouseList = async () => {
-    let { status, data: { list } } = await getListByFilter(this.cityId, this.filters, 1, 20);
+    let { status, data: { list, count } } = await getListByFilter(this.cityId, this.filters, 1, 20);
     if (status === 200) {
+      // 有数据的提示
+      if (count !== 0) {
+        Toast.success(`获取到${count}条房源数据！`)
+      }
       this.setState({
-        list
+        list,
+        count
       })
     }
+  }
+
+  // 下拉加载更多
+  // 判断当前行是否加载完成
+  isRowLoaded = ({ index }) => {
+    return !!this.state.list[index];
+  }
+  // 回调：下拉到一定位置执行=》调用后台接口获取下一页数据=》刷新列表
+  loadMoreRows = ({ startIndex, stopIndex }) => {
+    return getListByFilter(this.cityId, this.filters, startIndex, stopIndex)
+      .then(res => {
+        // Store response data in list...
+        // console.log('下一页的数据：', startIndex, stopIndex, res)
+        if (res.status === 200) {
+          this.setState({
+            list: [...this.state.list, ...res.data.list]
+          })
+        }
+      })
+  }
+
+  // 渲染列表
+  renderList = () => {
+    const { count } = this.state;
+    return count > 0 ? <InfiniteLoader
+      minimumBatchSize={10}
+      isRowLoaded={this.isRowLoaded}
+      loadMoreRows={this.loadMoreRows}
+      rowCount={this.state.count}
+    >
+      {({ onRowsRendered, registerChild }) => (
+        <AutoSizer>
+          {({ height, width }) => (
+            <List
+              height={height}
+              onRowsRendered={onRowsRendered}
+              ref={registerChild}
+              className={styles.houseList}
+              rowCount={this.state.count}
+              rowHeight={130}
+              rowRenderer={this.renderHouseItem}
+              width={width}
+            />
+          )}
+        </AutoSizer>
+      )}
+    </InfiniteLoader> : <NoHouse>暂无房源数据</NoHouse>
   }
 
   render() {
@@ -78,18 +143,7 @@ export default class HouseList extends React.Component {
         <Filter onFilter={this.onFilter} />
         {/* 筛选结果：列表 */}
         {/* 列表 */}
-        <AutoSizer>
-          {({ height, width }) => (
-            <List
-              height={height}
-              className={styles.houseList}
-              rowCount={this.state.list.length}
-              rowHeight={130}
-              rowRenderer={this.renderHouseItem}
-              width={width}
-            />
-          )}
-        </AutoSizer>
+        {this.renderList()}
       </div>
     )
   }
